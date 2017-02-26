c96013d64b7eabf67ac049936e5e0b5fdb33f77d
server <server@seenada.thereyougo.co>
2017-02-26 20:32:32 +0000

    Add signup endpoint that accepts POST only

--- v1/api.py
+++ v1/api.py
@@ -1,12 +1,26 @@
-from flask import Flask
+import hashlib
+from flask import Flask,request,jsonify,abort
+from DbAccess import DbAccess
 
 app = Flask(__name__)
+db = DbAccess()
 
 # basic homepage
 @app.route('/')
 def home():
     return "Welcome to the page"
 
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    print "we got here"
+    content = request.get_json(force=True)
+    print content
+    username = content['username']
+    password = content['password']
+    hashed_password = hashlib.sha512(password).hexdigest()
+    db.insert_user(username, hashed_password) 
+    return "added"
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
