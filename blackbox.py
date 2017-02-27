30499eeb3148a4659e0aa764b69d84e5dc4e507d
Jocelyn Ramirez <jsyramirez@gmail.com>
2017-02-26 20:29:35 -0800

    Create blackbox.py
    Skeleton for dec/enc scheme.

---
+++ blackbox.py
@@ -0,0 +1,19 @@
+class BlackBox:
+
+	def encrypt_message(self, message, key):
+		return
+
+	def decrypt_message(self, ciphertext, key):
+		return
+
+	def encrypt_login(self, username, password, key):
+		return
+
+	def decrypt_login(self, cipher_username, cipher_password, key):
+		return
+
+	def __init__(self):
+		return
+
+def main():
+    bb = BlackBox()
