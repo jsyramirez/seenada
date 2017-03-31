a03250b1a71d6bbc1a3a506a5dd3d34fcbfd10a8
Thanh Nhut Tran <thanhnhut90@gmail.com>
2017-03-30 23:22:13 -0700

    added more code in

--- v2/DbAccess.py
+++ v2/DbAccess.py
@@ -25,25 +25,98 @@ class DbAccess(object):
                                             port=3306,\
                                             user='python',\
                                             passwd='123456',\
-                                            db='seenada_v1')
+                                            db='seenada_v2')
             self.__cursor = self.__conn.cursor()
             return 'ok'
-        except Exception as error:
+        except pymysql.Error as error:
             LOGGER.exception(error)
             return 'error'
 
+    def close_connection(self):
+        '''close connection'''
+        if self.__conn.open:
+            self.__conn.close()
+
     def add_user(self, username, salt, hashed_pwd):
         '''add user into database'''
-        return True
+        if self.get_connection() == 'ok':
+            try:
+                self.__cursor.execute("insert into users(username) value (%s)", (username,))
+                self.__cursor.execute("select * from users where username = %s", (username,))
+                result = self.__cursor.fetchone()
+                if result is not None:
+                    values = (result[0], salt, hashed_pwd)
+                    self.__cursor.execute("insert into pwd values (%s, %s, %s)", values)
+                    self.__conn.commit()
+                    return 'ok'
+                else:
+                    LOGGER.error('Insert failed')
+                    return 'error'
+            except pymysql.Error as error:
+                LOGGER.exception(error)
+                return 'error'
+            finally:
+                self.close_connection()
+        else:
+            return 'no connection'
 
     def get_user(self, username):
         '''get user from username'''
-        return True
+        if self.get_connection() == 'ok':
+            try:
+                self.__cursor.execute("select users.id, users.username, pwd.salt, pwd.hashed_pwd\
+                                       from users inner join pwd on users.id = pwd.id\
+                                       where username = %s", (username,))
+                result = self.__cursor.fetchone()
+                if result is not None:
+                    return {'id': result[0], 'username': result[1],
+                            'salt': result[2], 'hashed_pwd': result[3]}
+                else:
+                    LOGGER.debug('No result from query')
+                    return {}
+            except pymysql.Error as err:
+                LOGGER.exception(err)
+                return 'error'
+            finally:
+                self.close_connection()
+        else:
+            return 'no connection'
 
     def add_message(self, message_obj):
         '''add message into database'''
-        return True
+        if self.get_connection() == 'ok':
+            try:
+                values = (message_obj['from'], message_obj['to'],
+                          message_obj['message'], message_obj['time'], 1)
+                self.__cursor.execute("insert into messages\
+                                      (fromUsr, toUsr, content, timestamp, isRead)\
+                                      values (%s, %s, %s, %s, %s)", values)
+                self.__conn.commit()
+                return 'ok'
+            except pymysql.Error as err:
+                LOGGER.exception(err)
+                return 'error'
+            finally:
+                self.close_connection()
+        else:
+            return 'no connection'
 
     def get_unread_message(self, username):
         '''get unread message for a user'''
-        return True
+        if self.get_connection() == 'ok':
+            try:
+                self.__cursor.execute("select * from messages\
+                                       where username = %s and isRead = 0", (username,))
+                result = self.__cursor.fetchall()
+                if result is None:
+                    return 'No new message'
+                else:
+                    return {"from": result[1], "to": result[2],
+                            "message": result[3], "timestamp": result[4]}
+            except pymysql.Error as err:
+                LOGGER.exception(err)
+                return 'error'
+            finally:
+                self.close_connection()
+        else:
+            return 'no connection'
--- v2/api.py
+++ v2/api.py
@@ -7,6 +7,7 @@ import binascii
 import logging
 from flask import Flask, request, jsonify, abort
 import scrypt
+from DbAccess import DbAccess
 
 LOGGER = logging.getLogger("api")
 LOGGER.setLevel(logging.INFO)
@@ -17,6 +18,7 @@ HANDLER.setFormatter(FORMATTER)
 LOGGER.addHandler(HANDLER)
 
 APP = Flask(__name__)
+DB = DbAccess()
 
 @APP.route("/")
 def hello():
@@ -27,13 +29,17 @@ def hello():
 def register():
     '''new user sign up'''
     content = request.get_json(force=True)
-    username = content['username']
+    username = (content['username']).encode('ascii', 'ignore')
     pwd = (content['password']).encode('ascii', 'ignore')
     salt = uuid.uuid4().hex
     hashed_binary_pwd = scrypt.hash(pwd, salt)
     hashed_pwd = binascii.hexlify(hashed_binary_pwd)
-    LOGGER.info(hashed_pwd)
-    return jsonify({"username": username, "password": hashed_pwd, "salt": salt})
+    response = DB.add_user(username, salt, hashed_pwd)
+    if response == 'ok':
+        return "Added successfully"
+    else:
+        LOGGER.error(response)
+        abort(500)
     #need to:
     #insert username into username table and get id out
     #insert id, salt, hashed password into pwd table
@@ -43,9 +49,12 @@ def first_signin():
     '''first signin, return salt'''
     content = request.get_json(force=True)
     username = content['username']
-    #what need to be done:
-    #query userid and then result salt with challenge c
-    return True
+    user = DB.get_user(username)
+    if isinstance(user, dict):
+        challenge = uuid.uuid4().hex
+        return jsonify({"salt": user['salt'], "challenge": challenge})
+    else:
+        return "no user found"
 
 @APP.route("/signin2", methods=['POST'])
 def second_signin():
@@ -54,12 +63,12 @@ def second_signin():
     username = content['username']
     tag = content['tag']
     challenge = content['challenge']
-    #what need to be done
-    #based on username, get userid and query hashed_password
-    #key = hashed_password
-    new_tag = hmac.new('sharedkey', challenge, hashlib.sha512)
+    user = DB.get_user(username)
+    new_tag = hmac.new(user['hashed_pwd'], challenge, hashlib.sha512)
     if hmac.compare_digest(tag, new_tag):
-        return True
+        return "Sign in"
+    else:
+        abort(403)
 
 @APP.route("/get_message", methods=['GET'])
 def get_message():
